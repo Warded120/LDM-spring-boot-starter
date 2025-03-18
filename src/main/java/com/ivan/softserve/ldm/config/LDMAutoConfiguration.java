5dff574f708089c5acb81afe70be7efd4099804d
Warded120 <greenvania11@gmail.com>
2025-03-18 16:39:30 +0200

    updated auto configuration class

--- src/main/java/com/ivan/softserve/ldm/config/LDMAutoConfiguration.java
+++ src/main/java/com/ivan/softserve/ldm/config/LDMAutoConfiguration.java
@@ -1,12 +1,9 @@
 package com.ivan.softserve.ldm.config;
 
-import com.ivan.softserve.ldm.controller.LogFileController;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.ComponentScan;
 
 @AutoConfiguration
-@ConditionalOnClass(LogFileController.class)
-@ComponentScan(basePackageClasses = {LogFileController.class})
+@ComponentScan(basePackages = "com.ivan.softserve.ldm")
 public class LDMAutoConfiguration {
 }
