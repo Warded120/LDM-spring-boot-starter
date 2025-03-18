6911544059945d3f9044e621565b6ac2dbd43b55
Warded120 <greenvania11@gmail.com>
2025-03-18 18:13:28 +0200

    changed LogFileController to request secretKey as header instead of body

--- src/main/java/com/ivan/softserve/ldm/controller/LogFileController.java
+++ src/main/java/com/ivan/softserve/ldm/controller/LogFileController.java
@@ -5,6 +5,7 @@ import com.ivan.softserve.ldm.constant.HttpStatuses;
 import com.ivan.softserve.ldm.dto.PageableDto;
 import com.ivan.softserve.ldm.dto.logs.LogFileMetadataDto;
 import com.ivan.softserve.ldm.dto.logs.LogFileRequestDto;
+import com.ivan.softserve.ldm.dto.logs.filter.LogFileFilterDto;
 import com.ivan.softserve.ldm.service.dotenv.DotenvService;
 import com.ivan.softserve.ldm.service.log.file.LogFileService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,6 +16,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
@@ -22,13 +24,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @Lazy
 @RequestMapping("/logs")
 public class LogFileController {
@@ -54,20 +59,18 @@ public class LogFileController {
     @ApiPageable
     @PostMapping
     public ResponseEntity<PageableDto<LogFileMetadataDto>> listLogFiles(
+        @RequestHeader @NotNull String secretKey,
         @Schema(
             description = "Filters for logs",
             name = "LogFileFilterDto",
             type = "object",
-            example = LogFileRequestDto.defaultJson) @RequestBody @Valid LogFileRequestDto requestDto,
+            example = LogFileFilterDto.defaultJson) @RequestBody @Valid LogFileFilterDto filterDto,
         @Parameter(hidden = true) Pageable page) {
         return ResponseEntity.status(HttpStatus.OK)
-            .body(logFileService.listLogFiles(page, requestDto.filterDto(), requestDto.secretKey()));
+            .body(logFileService.listLogFiles(page, filterDto, secretKey));
     }
 
-    @Operation(summary = "Returns content of a file with given filename",
-        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-            content = @Content(mediaType = "text/plain",
-                schema = @Schema(type = "string"))))
+    @Operation(summary = "Returns content of a file with given filename")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
             content = @Content(schema = @Schema(example = "string"))),
@@ -82,16 +85,13 @@ public class LogFileController {
     })
     @PostMapping("/view/{filename}")
     public ResponseEntity<String> viewLogFileContent(
-        @RequestBody String secretKey,
+        @RequestHeader @NotNull String secretKey,
         @PathVariable String filename) {
         return ResponseEntity.status(HttpStatus.OK)
             .body(logFileService.viewLogFileContent(logFileService.sanitizeFilename(filename), secretKey));
     }
 
-    @Operation(summary = "Returns a url that triggers file download in a browser",
-        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-            content = @Content(mediaType = "text/plain",
-                schema = @Schema(type = "string"))))
+    @Operation(summary = "Returns a url that triggers file download in a browser")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = HttpStatuses.OK,
             content = @Content(schema = @Schema(example = HttpStatuses.OK))),
@@ -104,7 +104,7 @@ public class LogFileController {
     })
     @PostMapping("/download/{filename}")
     public ResponseEntity<Resource> downloadLogFile(
-        @RequestBody String secretKey,
+        @RequestHeader @NotNull String secretKey,
         @PathVariable String filename) {
         return ResponseEntity.ok()
             .contentType(MediaType.APPLICATION_OCTET_STREAM)
@@ -128,7 +128,7 @@ public class LogFileController {
     })
     @PostMapping("/delete-dotenv")
     public ResponseEntity<Object> deleteDotenvFile(
-        @RequestBody String secretKey) {
+        @RequestHeader @NotNull String secretKey) {
         dotenvService.deleteDotenvFile(secretKey);
         return ResponseEntity.ok().build();
     }
