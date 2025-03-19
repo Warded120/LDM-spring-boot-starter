1f5e8b0cadb7daf8bdfe26ef8b4a537d8eaeb2f7
Warded120 <greenvania11@gmail.com>
2025-03-19 10:59:34 +0200

    added @ConditionalOnMissingBean annotations on configurations

--- src/main/java/com/ivan/softserve/ldm/config/DotenvConfig.java
+++ src/main/java/com/ivan/softserve/ldm/config/DotenvConfig.java
@@ -5,12 +5,14 @@ import com.ivan.softserve.ldm.constant.ErrorMessage;
 import com.ivan.softserve.ldm.exception.exceptions.FunctionalityNotAvailableException;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @Lazy
+@ConditionalOnMissingBean(DotenvConfig.class)
 public class DotenvConfig {
     @Bean
     Dotenv dotenv() {
--- src/main/java/com/ivan/softserve/ldm/config/SecurityConfig.java
+++ src/main/java/com/ivan/softserve/ldm/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.ivan.softserve.ldm.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@ConditionalOnMissingBean(SecurityConfig.class)
 public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
